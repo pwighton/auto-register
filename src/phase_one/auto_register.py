519e53518b1dbbe0728bd40dbd56be9aeca2959d
Oliver Hinds <ohinds@nmr.mgh.harvard.edu>
2014-08-14 11:26:26 -0400

    Main loop checks for new images received from the scanner and for keyboard input. Shuts down cleanly.

--- src/phase_one/auto_register.py
+++ src/phase_one/auto_register.py
@@ -3,27 +3,72 @@
 import argparse
 import os
 import sys
+import thread
+import traceback
 
-import ext.receive_nii
+from ext import receive_nii
+from util import TerminalInput
 
 class AutoRegister:
 
-    def __init__(self):
+    def __init__(self, args):
         self._should_shutdown = False
+
+        # unused but required reciever args
+        args.four_dimensional = False
+        args.single_series = False
+
         self._receiver = receive_nii.ImageReceiver(args)
+        self._term_input = TerminalInput()
+
         self._receiver.start()
+        self._term_input.start()
+
+    def check_for_input(self):
+        c = self._term_input.get_char()
+        if c == None:
+            return
+
+        if c == 'q':
+            self.shutdown()
+
 
     def run(self):
+        print "Running AutoRegister, 'q' to quit"
+
         while self._receiver.is_running() and not self._should_shutdown:
+            self.check_for_input()
+
             filename = self._receiver.get_next_filename()
             if filename is not None:
                 print "saw file %s" % filename
 
                 # TODO handle the file
 
+    def shutdown(self):
+        self._should_shutdown = True
+        self._term_input.stop()
+        self._receiver.stop()
+        print "Shuting down"
 
 def main(args):
-    AutoRegister ar
+    def verifyPathExists(path):
+        if not os.path.exists(path):
+            raise ArgumentError()
+        return path
+
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--save_directory', type=verifyPathExists,
+                        help='Directory in which to save images and data')
+    parser.add_argument('-H', '--host', default='localhost',
+                        help='Address of the scanner from which to listen '
+                        'for images [localhost]')
+    parser.add_argument('-p', '--port', default=15001, type=int,
+                        help='On the scanner address from which to listen '
+                        'for images [15001]')
+
+    ar = AutoRegister(parser.parse_args())
     ar.run()
     return 0
 
---
+++ src/phase_one/util.py
@@ -0,0 +1,58 @@
+import fcntl
+import os
+import sys
+import termios
+import threading
+
+class TerminalInput:
+
+    def __init__(self):
+
+        # setup the terminal for interative character capture
+        self._fd = sys.stdin.fileno()
+
+        self._oldterm = termios.tcgetattr(self._fd)
+        newattr = termios.tcgetattr(self._fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(self._fd, termios.TCSANOW, newattr)
+
+        self.oldflags = fcntl.fcntl(self._fd, fcntl.F_GETFL)
+        fcntl.fcntl(self._fd, fcntl.F_SETFL, self.oldflags | os.O_NONBLOCK)
+
+        self._chars = []
+        self._thread = None
+        self._mutex = threading.Lock()
+        self._should_stop = False
+
+    def start(self):
+        self._thread = threading.Thread(target=self.run)
+        self._thread.start()
+
+    def run(self):
+        while not self._should_stop:
+            try:
+                c = sys.stdin.read(1)
+
+                self._mutex.acquire()
+                self._chars.append(c)
+                self._mutex.release()
+            except IOError:
+                pass
+
+    def stop(self):
+        self._should_stop = True
+        self._thread.join()
+        print "Resetting terminal"
+        termios.tcsetattr(self._fd, termios.TCSAFLUSH, self._oldterm)
+        fcntl.fcntl(self._fd, fcntl.F_SETFL, self.oldflags)
+
+    def get_char(self):
+
+        self._mutex.acquire()
+        if len(self._chars) == 0:
+            c = None
+        else:
+            c = self._chars.pop()
+        self._mutex.release()
+
+        return c
